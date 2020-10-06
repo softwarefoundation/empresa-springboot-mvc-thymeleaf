27ed1fb860d6252a7f5e3052a37053f8a058af98
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-06 17:14:10 -0400

    Salvando departamento no BD

--- src/main/java/com/softwarefoundation/empresaspringbootmvcthymeleaf/web/controller/DerpartamentoController.java
+++ src/main/java/com/softwarefoundation/empresaspringbootmvcthymeleaf/web/controller/DerpartamentoController.java
@@ -1,21 +1,34 @@
 package com.softwarefoundation.empresaspringbootmvcthymeleaf.web.controller;
 
+import com.softwarefoundation.empresaspringbootmvcthymeleaf.entity.Departamento;
+import com.softwarefoundation.empresaspringbootmvcthymeleaf.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DerpartamentoController {
 
+    @Autowired
+    private DepartamentoService departamentoService;
+
     @GetMapping("cadastrar")
-    public String cadastrar() {
+    public String cadastrar(Departamento departamento) {
         return "departamento/cadastro";
     }
 
     @GetMapping("listar")
     public String listar() {
         return "departamento/lista";
+    }
+
+    @PostMapping(path = "salvar")
+    public String salvar(Departamento departamento){
+        departamentoService.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 
 }
