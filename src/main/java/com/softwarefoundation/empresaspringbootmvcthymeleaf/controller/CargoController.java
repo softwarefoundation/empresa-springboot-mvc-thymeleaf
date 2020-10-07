f9202fef0847ee0da344b34b1fae77e4f29cef0b
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-07 09:17:59 -0400

    Add funcionalidade aalterar cargo

--- src/main/java/com/softwarefoundation/empresaspringbootmvcthymeleaf/controller/CargoController.java
+++ src/main/java/com/softwarefoundation/empresaspringbootmvcthymeleaf/controller/CargoController.java
@@ -8,10 +8,7 @@ import com.softwarefoundation.empresaspringbootmvcthymeleaf.util.Mensagem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -29,6 +26,20 @@ public class CargoController {
     @GetMapping(path = "/cadastrar")
     public String cadastrar(Cargo cargo){
         return "/cargo/cadastro";
+    }
+
+
+    @GetMapping(path = "/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model){
+        model.addAttribute("cargo", cargoService.pesquisarPorId(id));
+        return "/cargo/cadastro";
+    }
+
+    @PostMapping(path = "/editar")
+    public String editar(Cargo cargo, RedirectAttributes attr){
+        cargoService.editar(cargo);
+        Mensagem.Sucesso.registroSalvoComsucesso(attr);
+        return "redirect:/cargos/cadastrar";
     }
 
     @GetMapping(path = "/listar")
